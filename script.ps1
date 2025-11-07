737ed63481e593536e1b45cd5b8093ccf74d2e04
PineTel <gianlucaam@edu.aau.at>
2025-11-07 11:08:50 +0100

    Simple admin check

--- script.ps1
+++ script.ps1
@@ -1,2 +1,1 @@
 ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
