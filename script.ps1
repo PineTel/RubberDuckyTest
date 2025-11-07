7fa78d2196afc4914512ba16eca1c812242fb435
PineTel <gianlucaam@edu.aau.at>
2025-11-07 11:07:35 +0100

    Test2

--- script.ps1
+++ script.ps1
@@ -1,1 +1,2 @@
 ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
